b0387cc7aa1eaf13f102c661073629a02ba28d9a
PPeltola <murderbanana@gmail.com>
2019-11-10 21:57:07 +0200

    broke checkstyle

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -23,7 +23,8 @@ public class Varasto {
             this.saldo = 0.0;
         } else if (alkuSaldo <= tilavuus) {
             this.saldo = alkuSaldo;
-        } else {
+        } else 
+	{
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
         }
     }
